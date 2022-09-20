4c2e7580b9b6a4b55fb715d922afdfcc6504a5b9
Ana <anais.chazelas91@gmail.com>
2022-09-20 23:00:04 +0200

    Day 14

--- 1-10/netflix-rep/script.js
+++ 1-10/netflix-rep/script.js
@@ -3,3 +3,11 @@
 function onClick() {
     document.querySelector('img.pic-pic').style.border = "solid 2px red";
 }
+
+function changePic() {
+    document.getElementById("change").src="../../img/illustrations/dragons.jpeg";
+}
+
+function initPic() {
+    document.getElementById("change").src="../../img/illustrations/mrrobot.jpeg";
+}
